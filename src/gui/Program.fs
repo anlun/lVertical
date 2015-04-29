8ff54d35148c1d78592195f113d55f04ddeae761
SergeevED <sergeev.ev.dm@gmail.com>
2015-04-29 11:54:47 +0300

    changed_previous_step_button

--- src/gui/Program.fs
+++ src/gui/Program.fs
@@ -20,38 +20,20 @@ let programLabel =
   lbl
 
 let mutable env       : string -> Option<int> = fun (s : string) -> None
+let mutable prevEnv   : List<string -> Option<int>> = []
 let mutable program   : Option<Stmt.t> = None 
-let mutable prevSteps : List<Stmt.t> = []
-
-let prevStepAction (but : Button) args =
-  match prevSteps with
-  | [] -> but.Enabled <- false
-  | lastStep :: tail -> 
-    program <- Some lastStep
-    prevSteps <- tail
-    programLabel.Text <- sprintf "%A" program
-    if prevSteps = [] then but.Enabled <- false
-
-let prevStepButton =
-  let but = new Button()
-  but.Text     <- "Prev Step"
-  but.Location <- System.Drawing.Point(programInput.Width - 2 * but.Width, programInput.Height)
-  but.Enabled  <- false
-  but.Click.Add (prevStepAction but)
-  but
+let mutable prevProgr : List<Stmt.t> = []
 
 let nextStepAction (but : Button) args =
   match program with 
-  | None   -> 
-    but.Enabled <- false
-    prevStepButton.Enabled <- false
+  | None   -> but.Enabled <- false
   | Some p ->
-    prevSteps <- p :: prevSteps
+    prevProgr <- p :: prevProgr
+    prevEnv <- env :: prevEnv
     let (nenv, np) = ss env p
     env     <- nenv
     program <- np
     programLabel.Text <- sprintf "%A" program
-    prevStepButton.Enabled <- true
 
 let nextStepButton =
   let but = new Button()
@@ -61,12 +43,32 @@ let nextStepButton =
   but.Click.Add (nextStepAction but)
   but
 
+let prevStepAction (but : Button) args =
+  match prevProgr with
+  | [] -> MessageBox.Show("There is no previous steps") |> ignore
+  | lastStep :: tail -> 
+    program <- Some lastStep
+    env <- prevEnv.Head
+    prevProgr <- tail
+    prevEnv <- prevEnv.Tail
+    programLabel.Text <- sprintf "%A" program
+    nextStepButton.Enabled <- true
+
+let prevStepButton =
+  let but = new Button()
+  but.Text     <- "Prev Step"
+  but.Location <- System.Drawing.Point(programInput.Width - 2 * but.Width, programInput.Height)
+  but.Enabled  <- false
+  but.Click.Add (prevStepAction but)
+  but
+
 let interpretAction args =
   let parseResult = &programInput.Text |> parse ()
   try
     program <- parseResult |> List.head |> fst |> Some
     env <- (fun s -> None)
     nextStepButton.Enabled <- true
+    prevStepButton.Enabled <- true
     programLabel.Text <- sprintf "%A" program
   with
   | _ -> failwith "test"
